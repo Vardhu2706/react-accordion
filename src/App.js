120607f80169229b76afc1220703c7280ea3bcf3
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-30 09:57:39 +0530

    Bug fixes.

--- src/App.js
+++ src/App.js
@@ -7,14 +7,12 @@ import SingleQuestion from "./Question";
 
 // Functional Component
 function App() {
-  const [questions, setQuestions] = useState(data);
-
   return (
     <main>
       <div className="container">
         <h3>Questions And Answers About Login</h3>
         <section className="info">
-          {questions.map((question) => {
+          {data.map((question) => {
             return <SingleQuestion key={question.id} {...question} />;
           })}
         </section>
