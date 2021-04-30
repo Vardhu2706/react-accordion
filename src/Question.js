df2d6fca9088b4393f9bc91676c206a91ff5da76
Vardhu2706 <ayyagari.vardhu@gmail.com>
2021-04-30 09:34:10 +0530

    Initial Project Setup

---
+++ src/Question.js
@@ -0,0 +1,13 @@
+// Importing Helpers
+import React, { useState } from "react";
+import { AiOutlineMinus, AuOutlinePlus } from "react-icons/ai";
+
+// Importing Components/ Files
+
+// Functional Component
+const Question = () => {
+  return <h2>Question Component</h2>;
+};
+
+// Default Export
+export default Question;
